98b197d928655594164bc9a3063b913d3a573101
Meighel <meighelnicolle@gmail.com>
2024-10-28 08:51:22 +0800

    search and add columns

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -2,13 +2,42 @@ from django.contrib import admin
 
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college",)
+    search_fields = ("college")
+
+@admin.register(Program)
+class ProgamAdmin(admin.ModelAdmin):
+    list_display = ("program", "get_college",)
+    search_fields = ("program")
+
+    @admin.display(description="College")
+    def get_college(self, obj):
+        try:
+            college_name = College.objects.get(id=obj.college)
+            return college_name.college
+        except College.DoesNotExist:
+            return None
+
+@admin.register(Organization)
+class Organization(admin.ModelAdmin):
+    list_display = ("organization", "get_college",)
+    search_fields = ("organization")
+
+    @admin.display(description="College")
+    def get_college(self, obj):
+        try:
+            college_name = College.objects.get(id=obj.college)
+            return college_name.college
+        except College.DoesNotExist:
+            return None
+    
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname",)
+
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
     list_display = ("student", "get_member_program", "organization", "date_joined",)
